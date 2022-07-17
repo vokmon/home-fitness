106bf7bc76db6f7fadd8cd0019cd1346379d4147
Arnon  Ruangthanawes <arnon_r@hotmail.co.nz>
2022-07-17 11:41:04 +0700

    Update exercise complete card

--- src/components/activities/ExerciseComplete.js
+++ src/components/activities/ExerciseComplete.js
@@ -1,14 +1,68 @@
-import { Fade } from '@mui/material';
-import React from 'react';
+import PropTypes from 'prop-types';
+import { Stack, Fade, Typography } from '@mui/material';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import React, { useEffect, useState } from 'react';
+import { getRandomInt } from '../../services/ExerciseServiceHelper';
+import { FacebookIcon, FacebookShareButton, LineIcon, LineShareButton } from 'react-share';
+import { URL } from '../../routes/RouteConstants';
 
-function ExerciseComplete() {
+// https://home-fitness.netlify.app/session
+const url = `${window.location.origin}${URL.SESSION_URL}`;
+
+function ExerciseComplete({
+  exerciseDurationInSecond,
+}) {
+  const [imgUrl, setImgUrl] = useState(null);
+  const getRandomImage = async () => {
+    const random = getRandomInt(1, 11);
+    const imageUrl = await import(`../../assets/images/quotes/${random}.jpeg`);
+    setImgUrl(imageUrl.default);
+  };
+
+  useEffect(() => {
+    getRandomImage();
+  }, []);
+
   return (
     <Fade in>
-      <div style={{ width: '100%', height: '50vh'}}>
-        Complete
-      </div>
+      <Stack flexDirection='column' alignItems='center' sx={{ width: '100%', height: '50vh' }}>
+        <Typography variant='h4' gutterBottom><LocalFireDepartmentIcon sx={{ fill: 'orange' }} /> Congratulations <LocalFireDepartmentIcon sx={{ fill: 'orange' }} /></Typography>
+        <Typography>You have finished {exerciseDurationInSecond / 60} minute exercise</Typography>
+
+        <br />
+
+        {imgUrl && (
+          <img
+            src={imgUrl}
+            srcSet={imgUrl}
+            alt={imgUrl}
+            loading='lazy'
+            style={{
+              width: '400px',
+              maxWidth: '70vw',
+              maxHeight: '400px'
+            }}
+          />
+        )}
+        <br />
+        <Typography>Share to my success</Typography>
+        <Stack flexDirection='row' alignItems='center' gap={2}>
+          <FacebookShareButton
+            url={url}
+          >
+            <FacebookIcon size={48} round />
+          </FacebookShareButton>
+
+          <LineShareButton url={url}>
+            <LineIcon size={48} round />
+          </LineShareButton>
+        </Stack>
+      </Stack>
     </Fade>
   );
 }
 
+ExerciseComplete.propTypes = {
+  exerciseDurationInSecond: PropTypes.number.isRequired,
+};
 export default ExerciseComplete;
--- src/components/activities/ExerciseSession.js
+++ src/components/activities/ExerciseSession.js
@@ -54,7 +54,7 @@ function ExerciseSession({
 
   if (isCompleted) {
     return (
-      <ExerciseComplete />
+      <ExerciseComplete exerciseDurationInSecond={exerciseDurationInSecond} />
     );
   }
   return (
